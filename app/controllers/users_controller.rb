15dfa5b318778713c9dcf3d6b65bb81a8691aab6
Doris Morales <moralesdoris880@gmail.com>
2022-03-22 13:00:33 -0400

    commit tree cbe162f8b5c3c78d216f8938805a28357d138244

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show 
+        user = User.find_by(id:params[:id])
+        if user
+            render json: user, include: :items, status: :ok
+        else
+            render json: {error: "Not Found"}, status: :not_found
+        end
+    end
 end
